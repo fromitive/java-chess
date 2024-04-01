925214748f412a5696edc98e58081048a7956204
fromitive <fromitive@gmail.com>
2024-04-01 11:23:09 +0900

    refactor: Application, ChessGame 가독성 개선
    불필요한 필드 제거

--- src/main/java/chess/Application.java
+++ src/main/java/chess/Application.java
@@ -13,8 +13,12 @@ public class Application {
     public static void main(String[] args) {
         try (var scanner = new Scanner(System.in);
              var connection = MySQLConfiguration.getConnection()) {
-            ChessGame chessGame = new ChessGame(new InputView(scanner), new OutputView());
-            chessGame.start(new ChessService(new DatabaseChessDAO(connection)));
+            InputView inputView = new InputView(scanner);
+            OutputView outputView = new OutputView();
+            ChessService chessService = new ChessService(new DatabaseChessDAO(connection));
+            
+            ChessGame chessGame = new ChessGame();
+            chessGame.start(inputView, outputView, chessService);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/chess/game/ChessGame.java
+++ src/main/java/chess/game/ChessGame.java
@@ -7,23 +7,17 @@ import chess.view.input.InputView;
 import chess.view.output.OutputView;
 
 public class ChessGame {
-    private final InputView inputView;
-    private final OutputView outputView;
 
-    public ChessGame(final InputView inputView, final OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-    }
-
-    public void start(ChessService chessService) {
+    public void start(InputView inputView, OutputView outputView, ChessService chessService) {
         outputView.printInitialMessage();
         GameStatus gameStatus = new InitialGame();
         while (gameStatus.isPlayable()) {
-            gameStatus = getGameStatus(gameStatus, chessService);
+            gameStatus = getGameStatus(gameStatus, inputView, outputView, chessService);
         }
     }
 
-    private GameStatus getGameStatus(GameStatus gameStatus, ChessService chessService) {
+    private GameStatus getGameStatus(GameStatus gameStatus, InputView inputView, OutputView outputView,
+                                     ChessService chessService) {
         try {
             gameStatus = gameStatus.play(inputView, outputView, chessService);
         } catch (IllegalArgumentException e) {
